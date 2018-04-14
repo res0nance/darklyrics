01fcfefe95dcc88b5105872437423adec1ec9e63
Fluentifer <jakob.ledig@haw-hamburg.de>
2018-04-14 13:49:53 +0200

    changes to __init__.py  to access methods without darklyrcis.darklyrics

--- darklyrics/__init__.py
+++ darklyrics/__init__.py
@@ -1,3 +1,3 @@
 from .darklyrics import get_lyrics, get_all_lyrics, get_songs, get_albums, LyricsNotFound
 
-__all__ = ['get_lyrics', 'get_all_lyrics', 'get_albums', 'get_songs' 'LyricsNotFound']
+__all__ = ['get_lyrics', 'get_all_lyrics', 'get_albums', 'get_songs', 'LyricsNotFound']
